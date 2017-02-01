7fc5c23aa06acc37f56df2414c088772abb2a223
James Seden Smith <sedders123@gmail.com>
2017-02-01 11:31:46 +0000

    Ignore retweets

--- angrybirds/stream.py
+++ angrybirds/stream.py
@@ -1,10 +1,13 @@
 import tweepy
+import json
 
 
 class AngryBirdsStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
+        if "retweeted_status" in status._json:
+            return
+        print(json.dumps(status._json, sort_keys=True, indent=4))
 
     def on_error(self, status_code):
         if status_code == 420:
