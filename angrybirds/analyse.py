7429868e9a0b77b9c40a7b3dc931c9405b57bb18
James Seden Smith <sedders123@gmail.com>
2017-02-08 11:40:01 +0000

    Fixed analyisis code

--- angrybirds/analyse.py
+++ angrybirds/analyse.py
@@ -1,16 +1,16 @@
-import .mongo
-import .sentiment
+from .mongo import get_db, get_collection, insert_document
+from .sentiment import get_analysis
 
 
 def analyse_tweets():
-    db = mongo.get_db()
-    collection = mongo.get_collection('raw_tweets', db)
+    db = get_db()
+    collection = get_collection('raw_tweets', db)
     for tweet in collection.find({}):
-        mongo.insert_document({
+        insert_document({
             "id": tweet["id"],
             "text": tweet["text"],
             "user_id": tweet["user"]["id"],
             "user_followers_count": tweet["user"]["followers_count"],
             "timestamp": tweet["timestamp_ms"],
-            "sentiment": sentiment.get_analysis(tweet["text"])
+            "sentiment": get_analysis(tweet["text"])
         }, 'tweets')
